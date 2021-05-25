b46efb342d52e4cb552cf031f1b1b554890a828e
M16 <Adilmohamedmohamed97@gmail.com>
2021-05-25 15:21:50 +0100

    modif

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,25 @@
-import {Perso} from "./personne.js";
+import {Perso,Maison,Epicerie,Oignon,Oeuf,Epice,Fromage,Couteau,Bol,Poele} from "./obj.js";
+let perso = new Perso("Franck","",20,[],[]);
+let maison = new Maison("Maison",[]);
+let epicerie = new Epicerie("Epicerie",[],"Paniers");
+epicerie.paniers=[
+    {type:'Panier 1', bac : []},
+    {type:'Panier 2', bac : []},
+];
+let oignon = new Oignon("Oignon","Intact",1.10);
+let oeuf = new Oeuf("Oeuf","Intact",0.2);
+let epice = new Epice("Epice","Intact",0.8);
+let fromage = new Fromage("Fromage","Intact",0.9);
+let couteau = new Couteau("Couteau","Coupé");
+let bol = new Bol("Bol",[]);
+let poele = new Poele("Poele",[]);
+
+perso.seDeplacer(maison);
+
+perso.seDeplacer(epicerie);
+
+
+
+
+
 
---
+++ src/js/obj.js
@@ -0,0 +1,124 @@
+class Perso {
+    constructor(nom,lieu,argent,mainDroite,mainGauche){
+        this.nom = nom
+        this.lieu = lieu
+        this.argent = argent
+        this.mainDroite = mainDroite
+        this.mainGauche = mainGauche
+        this.seDeplacer=(lieu)=>{
+            lieu.personnes.push(Perso.nom)
+            console.log(`${this.nom} est actuellement à la ${lieu.nom}`);
+        }
+        this.payerArticle=(article)=>{
+            argent-=article.prix
+            console.log(`${article.nom} a couté ${article.prix} `);
+        }
+        this.couper=(ingredient, outil)=>{
+            ingredient.etats=outil.action
+            console.log(`${this.nom} a coupé ${this.nom}`);
+        }
+    }
+}
+
+export {Perso};
+
+///Lieu///
+
+class Lieu{
+    constructor(nom,personnes){
+        this.nom = nom;
+        this.personnes = personnes;
+    }
+}
+
+class Maison extends Lieu{
+    constructor(nom,personnes){
+        super(nom,personnes)
+    }
+}
+
+class Epicerie extends Lieu{
+    constructor(nom,personnes,paniers){
+        super(nom,personnes)
+        this.paniers = paniers;
+        this.ingredients = this.ingredients;
+    }
+}
+
+export {Lieu,Maison,Epicerie};
+
+///Ingrédients//
+
+class Ingredients {
+    constructor(nom,etats,prix){
+        this.nom = nom;
+        this.etats = etats;
+        this.prix = prix;
+    }
+}
+
+class Oignon extends Ingredients {
+    constructor(nom,etats,prix){
+        super(nom,etats,prix);
+    }
+}
+
+class Oeuf extends Ingredients {
+    constructor(nom,etats,prix){
+        super(nom,etats,prix);
+    }
+}
+
+class Epice extends Ingredients {
+    constructor(nom,etats,prix){
+        super(nom,etats,prix);
+    }
+}
+
+class Fromage extends Ingredients {
+    constructor(nom,etats,prix){
+        super(nom,etats,prix);
+    }
+}
+
+export {Ingredients, Oignon, Oeuf, Epice, Fromage};
+
+//Outils//
+
+class Outil{
+    constructor(nom){
+        this.nom = nom
+    }
+}
+
+class Couteau extends Outil {
+    constructor(nom,action){
+        super(nom),
+        this.action = action
+    }
+}
+
+class Bol extends Outil {
+    constructor(nom,tableau){
+        super(nom)
+        this.tableau = tableau,
+        this.meLanger=(nomMelanger)=>{
+            let newMelange = new ingredients(this.nom,"pas cuit!",2)
+        }
+    }
+    
+}
+
+class Poele extends Outil {
+    constructor(nom){
+        super(nom)
+        this.tableau = []
+        this.Cuir=()=>{
+            setTimeout(()=> { this.tableau[0]="cuit";
+                console.log(`blazblz`);
+            }, 4000)
+        }
+    }
+}
+
+export {Outil, Couteau, Bol, Poele};
